23a26ca65ce6fcb5e698429c4507d66d96f185e5
Guillaume Claudic <guillaume.claudic@gmail.com>
2015-11-07 15:18:12 +0100

    Ajout d'un cube dans la vue

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -21,11 +21,41 @@ void MainWindow::init()
 void MainWindow::draw()
 {
     glBegin(GL_QUADS);
-        glNormal3d(-1, 0, 0);
-        glVertex3d(-1, 1, 1);
-        glVertex3d(-1, -1, 1);
-        glVertex3d(-1, -1, -1);
-        glVertex3d(-1, 1, -1);
+        glColor3ub(255,0,0); //face rouge
+        glVertex3d(1,1,1);
+        glVertex3d(1,1,-1);
+        glVertex3d(-1,1,-1);
+        glVertex3d(-1,1,1);
+
+        glColor3ub(0,255,0); //face verte
+        glVertex3d(1,-1,1);
+        glVertex3d(1,-1,-1);
+        glVertex3d(1,1,-1);
+        glVertex3d(1,1,1);
+
+        glColor3ub(0,0,255); //face bleue
+        glVertex3d(-1,-1,1);
+        glVertex3d(-1,-1,-1);
+        glVertex3d(1,-1,-1);
+        glVertex3d(1,-1,1);
+
+        glColor3ub(255,255,0); //face jaune
+        glVertex3d(-1,1,1);
+        glVertex3d(-1,1,-1);
+        glVertex3d(-1,-1,-1);
+        glVertex3d(-1,-1,1);
+
+        glColor3ub(0,255,255); //face cyan
+        glVertex3d(1,1,-1);
+        glVertex3d(1,-1,-1);
+        glVertex3d(-1,-1,-1);
+        glVertex3d(-1,1,-1);
+
+        glColor3ub(255,0,255); //face magenta
+        glVertex3d(1,-1,1);
+        glVertex3d(1,1,1);
+        glVertex3d(-1,1,1);
+        glVertex3d(-1,-1,1);
     glEnd();
 }
 
