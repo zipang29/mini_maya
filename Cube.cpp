1e256c4ef4fe062d33531218a4c220affef2845d
Guillaume Claudic <guillaume.claudic@gmail.com>
2015-11-14 19:49:19 +0100

    Redimention du cube sur l'axe x (il reste encore quelques problèmes de fluidité de l'annimation mais sa fonctionne)

--- Cube.cpp
+++ Cube.cpp
@@ -17,41 +17,43 @@ void Cube::init()
     this->data = Parser::getDataMotion();
 
     // Initialisation des positions des vertex du cube
-    //face droite
-    x_FD_BD = 1;
-    y_FD_BD = -1;
-    z_FD_BD = 1;
-    x_FD_BG = 1;
-    y_FD_BG = -1;
-    z_FD_BG = -1;
-    x_FD_HG = 1;
-    y_FD_HG = 1;
-    z_FD_HG = -1;
-    x_FD_HD = 1;
-    y_FD_HD = 1;
-    z_FD_HD = 1;
+    x_G = 1;
+    y_G = -1;
+    z_G = 1;
+
+    x_C = 1;
+    y_C = -1;
+    z_C = -1;
+
+    x_B = 1;
+    y_B = 1;
+    z_B = -1;
+
+    x_F = 1;
+    y_F = 1;
+    z_F = 1;
 }
 
 void Cube::draw()
 {
     glBegin(GL_QUADS);
         glColor3ub(255, 0, 0); //face rouge => face haut
-        glVertex3d(1, 1, 1);
-        glVertex3d(1, 1, -1);
+        glVertex3d(x_F, y_F, z_F);
+        glVertex3d(x_B, y_B, z_B);
         glVertex3d(-1, 1, -1);
         glVertex3d(-1, 1, 1);
 
         glColor3ub(0, 255, 0); //face verte => face droite => FD
-        glVertex3f(x_FD_BD, y_FD_BD, z_FD_BD);
-        glVertex3f(x_FD_BG, y_FD_BG, z_FD_BG);
-        glVertex3f(x_FD_HG, y_FD_HG, z_FD_HG);
-        glVertex3f(x_FD_HD, y_FD_HD, z_FD_HD);
+        glVertex3f(x_G, y_G, z_G);
+        glVertex3f(x_C, y_C, z_C);
+        glVertex3f(x_B, y_B, z_B);
+        glVertex3f(x_F, y_F, z_F);
 
         glColor3ub(0, 0, 255); //face bleue => fasse bas
         glVertex3d(-1, -1, 1);
         glVertex3d(-1, -1, -1);
-        glVertex3d(1, -1, -1);
-        glVertex3d(1, -1, 1);
+        glVertex3d(x_C, y_C, z_C);
+        glVertex3d(x_G, y_G, z_G);
 
         glColor3ub(255, 255, 0); //face jaune => face gauche
         glVertex3d(-1, 1, 1);
@@ -60,13 +62,14 @@ void Cube::draw()
         glVertex3d(-1, -1, 1);
 
         glColor3ub(0, 255, 255); //face cyan => face derrière
-        glVertex3d(1, 1, -1);
-        glVertex3d(1, -1, -1);
+        glVertex3d(x_B, y_B, z_B);
+        glVertex3d(x_C, y_C, z_C);
         glVertex3d(-1, -1, -1);
         glVertex3d(-1, 1, -1);
+
         glColor3ub(255, 0, 255); //face magenta => face avant
-        glVertex3d(1, -1, 1);
-        glVertex3d(1, 1, 1);
+        glVertex3d(x_G, y_G, z_G);
+        glVertex3d(x_F, y_F, z_F);
         glVertex3d(-1, 1, 1);
         glVertex3d(-1, -1, 1);
     glEnd();
@@ -87,10 +90,10 @@ void Cube::animate()
 
         QVector<float> distance = this->data->calculDistance(&mainDroite1, &mainDroite2);
 
-        x_FD_BD = distance.at(0);
-        x_FD_BG = distance.at(0);
-        x_FD_HD = distance.at(0);
-        x_FD_HG = distance.at(0);
+        x_G = distance.at(0);
+        x_C = distance.at(0);
+        x_B = distance.at(0);
+        x_F = distance.at(0);
     }
 
     this->lineNumber++;
--- Cube.h
+++ Cube.h
@@ -20,10 +20,21 @@ class Cube
         DataMotion * data;
 
         //face droite
-        float x_FD_HG, y_FD_HG, z_FD_HG;
+        /*float x_FD_HG, y_FD_HG, z_FD_HG;
         float x_FD_HD, y_FD_HD, z_FD_HD;
         float x_FD_BG, y_FD_BG, z_FD_BG;
-        float x_FD_BD, y_FD_BD, z_FD_BD;
+        float x_FD_BD, y_FD_BD, z_FD_BD;*/
+
+        // Correspond aux vertex du cube. On part de la face avant dans le sens des aiguilles d'une montre en partant du point en haut à gauche de la
+        // face du devant du cube puise on fait pareille pour la face arrière
+        float x_A, y_A, z_A;
+        float x_B, y_B, z_B;
+        float x_C, y_C, z_C;
+        float x_D, y_D, z_D;
+        float x_E, y_E, z_E;
+        float x_F, y_F, z_F;
+        float x_G, y_G, z_G;
+        float x_H, y_H, z_H;
 };
 
 #endif // CUBE_H
